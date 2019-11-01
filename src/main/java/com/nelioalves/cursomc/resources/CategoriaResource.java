a550035c6ceebca7a3184fad0fc42c50fea6a420
Tomaz Silva <tomazrf@gmail.com>
2019-11-01 14:50:15 +0000

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -56,5 +58,16 @@ public class CategoriaResource {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(name="page", defaultValue="0", required=false)Integer page, 
+			@RequestParam(name="linesPerPage", defaultValue="24", required=false)Integer linesPerPage, 
+			@RequestParam(name="direction", defaultValue="ASC", required=false)String direction, 
+			@RequestParam(name="orderBy", defaultValue="nome", required=false)String orderBy) {
+		Page<Categoria> listPage = service.findPage(page, linesPerPage, direction, orderBy);
+		Page<CategoriaDTO> listPageDTO = listPage.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(listPageDTO);
 	}
 }
